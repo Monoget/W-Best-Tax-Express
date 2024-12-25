7b64d936d742864852118e5d0c0fecb49fb6bcf7
Monoget Saha <monoget2@gmail.com>
2024-12-26 00:37:07 +0600

    update all

---
+++ make-a-payment.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- meta tags -->
+    <meta charset="UTF-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- title -->
+    <title>Contact Us - Best Tax Express</title>
+
+    <!-- favicon -->
+    <link href="assets/img/logo/favicon.png" rel="icon" type="image/x-icon">
+
+    <?php include 'include/css.php'; ?>
+
+</head>
+
+<body class="home-2">
+
+<?php include 'include/header.php'; ?>
+
+
+<main class="main">
+
+    <!-- breadcrumb -->
+    <div class="site-breadcrumb" style="background: url(assets/img/breadcrumb/01.jpg)">
+        <div class="container">
+            <h2 class="breadcrumb-title">Tax Services, Including Tax Preparation Services & More</h2>
+            <ul class="breadcrumb-menu">
+                <li><a href="index.php">Home</a></li>
+                <li class="active">Tax Services</li>
+            </ul>
+        </div>
+    </div>
+    <!-- breadcrumb end -->
+
+
+    <!-- case-study single -->
+    <div class="case-single py-120">
+        <div class="container">
+            <div class="case-single-wrapper">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-4">
+                        <div class="case-sidebar">
+                            <div class="widget case-sidebar-content">
+                                <h4 class="case-sidebar-title">What We Do</h4>
+                                <ul>
+                                    <li>
+                                        TAX
+                                    </li>
+                                    <li>
+                                        BOOKKEEPING
+                                    </li>
+                                    <li>
+                                        PART-TIME CFO
+                                    </li>
+                                    <li>
+                                        FINANCIAL PLANNING
+                                    </li>
+                                    <li>
+                                        AUDIT-REVIEW-COMPILATION
+                                    </li>
+                                    <li>
+                                        CASH FLOW MANAGEMENT
+                                    </li>
+                                    <li>
+                                        BANK FINANCING
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-8 col-lg-8">
+                        <div class="case-details">
+                            <div class="case-details-img mb-30">
+                                <img alt="thumb" src="assets/img/case/single.jpg">
+                            </div>
+                            <div class="case-details">
+                                <h3 class="mb-20">Tax Filing</h3>
+                                <p class="mb-20">
+                                    According to a study released by the US Government's General Accounting Office, most
+                                    taxpayers (77% of 71 million taxpayers) believe they benefited from using a
+                                    professional tax preparation service for business tax prep, and personal tax prep.
+                                </p>
+                                <h3 class="mb-3">Tax Reduction Planning</h3>
+                                <p class="mb-20">
+                                    Discover how tax planning can help you reduce your tax liability and maximize your
+                                    after-tax income. Let's go beyond the basic tax preparation service and find
+                                    proactive tax-saving strategies together! Our firm also offers tax preparation
+                                    services, including business tax prep and personal tax prep.
+                                </p>
+                                <div class="row">
+                                    <div class="col-md-6 mb-20">
+                                        <img alt="" src="assets/img/case/01.jpg">
+                                    </div>
+                                    <div class="col-md-6 mb-20">
+                                        <img alt="" src="assets/img/case/02.jpg">
+                                    </div>
+                                </div>
+                                <h3 class="mb-3">Tax Resolution</h3>
+                                <p class="mb-20">
+                                    <b>Need help resolving a complex IRS dispute?</b><br/>
+                                    Getting a letter from the IRS is something that taxpayers rightfully dread. Hiring a
+                                    trusted CPA & advisory firm is the first step to a swift and favorable resolution
+                                    for tax disputes. Our tax professionals will review the dispute, and quickly assess
+                                    your position and that of the IRS. We also offer tax preparation services, including
+                                    business tax prep and personal tax prep.
+                                </p>
+                                <div class="my-4">
+                                    <div class="mb-3">
+                                        <h3 class="mb-3">Sales and Use Tax Filings</h3>
+                                        <p>
+                                            Attention small business owners! Are you struggling with sales tax
+                                            calculations? Our CPA Firm can help you navigate the complexities of sales
+                                            tax filings to ensure compliance in all jurisdictions. Contact us today for
+                                            personalized assistance. Our firm also offers tax preparation services,
+                                            including business tax prep and personal tax prep.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="my-4">
+                                    <h3 class="mb-3">Beneficial Owner Information (BOI)</h3>
+                                    <p>
+                                        Under the Corporate Transparency Act (CTA), businesses must submit a BOI report
+                                        detailing these individuals, enhancing transparency, and aiding in the
+                                        prevention of financial crimes such as money laundering, corruption, and tax
+                                        evasion. Our firm also offers tax preparation services, including business tax
+                                        prep and personal tax prep.
+                                    </p>
+                                </div>
+                                <div class="my-4">
+                                    <h3 class="mb-3">REPORT FOREIGN BANK AND FINANCIAL ACCOUNTS (FBAR)</h3>
+                                    <p>
+                                        Are you a U.S. citizen with foreign financial accounts exceeding $10,000? Don't
+                                        forget about FBAR (FinCEN Form 114) filing requirements! Stay compliant with the
+                                        law and avoid penalties. Schedule a free consultation with us today to ensure
+                                        peace of mind. Our firm also offers tax preparation services, including business
+                                        tax prep and personal tax prep.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- case-study single end -->
+</main>
+
+<?php include 'include/footer.php'; ?>
+
+
+<?php include 'include/js.php'; ?>
+
+</body>
+</html>
